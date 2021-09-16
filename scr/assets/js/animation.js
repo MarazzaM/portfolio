a7ee4d6492d08ef0c18beabe722498515462e6e6
LastLightbringer <89055276+MarazzaM@users.noreply.github.com>
2021-09-15 22:15:48 -0300

    slider added

--- scr/assets/js/animation.js
+++ scr/assets/js/animation.js
@@ -31,19 +31,4 @@ imagess.forEach((image) => {
   observer.observe(image);
 });
 
-//THIRD
-const imagesss = document.querySelectorAll(".anim4");
-
-observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.intersectionRatio > 0) {
-      entry.target.style.animation = `anim5 2s ${entry.target.dataset.delay} forwards ease-out`;
-    } else {
-      entry.target.style.animation = "none";
-    }
-  });
-});
-
-imagesss.forEach((image) => {
-  observer.observe(image);
-});
+//slider
