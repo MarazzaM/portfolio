cd03fee33cfc8325e765877b9d0b94118f55ca2b
LastLightbringer <89055276+MarazzaM@users.noreply.github.com>
2021-09-16 15:53:15 -0300

    coming soon easter egg

--- scr/assets/js/animation.js
+++ scr/assets/js/animation.js
@@ -1,13 +1,18 @@
 const images = document.querySelectorAll(".anim");
 
 observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
+  for (var i = 0; i < entries.length; i++){
+    if(entries[i].isIntersecting){
+      entries.forEach((entry) => {
     if (entry.intersectionRatio > 0) {
       entry.target.style.animation = `anim1 2s ${entry.target.dataset.delay} forwards ease-out`;
     } else {
       entry.target.style.animation = "none";
     }
-  });
+  })
+     
+    }
+  };
 });
 
 images.forEach((image) => {
@@ -18,13 +23,18 @@ images.forEach((image) => {
 const imagess = document.querySelectorAll(".anim2");
 
 observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
+  for (var i = 0; i < entries.length; i++){
+    if(entries[i].isIntersecting){ //add code of intersecting
+      entries.forEach((entry) => {
     if (entry.intersectionRatio > 0) {
       entry.target.style.animation = `anim3 2s ${entry.target.dataset.delay} forwards ease-out`;
     } else {
       entry.target.style.animation = "none";
     }
-  });
+  })
+     
+    }
+  };
 });
 
 imagess.forEach((image) => {
