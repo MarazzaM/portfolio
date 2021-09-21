900ed711809e8aa7bfed15262286ee82c823a68b
LastLightbringer <89055276+MarazzaM@users.noreply.github.com>
2021-09-21 18:48:19 -0300

    UPDATED LETTER ANIMATION

--- assets/js/main.js
+++ assets/js/main.js
@@ -281,62 +281,64 @@ imagess.forEach((image) => {
   observer.observe(image);
 });
 
-//slider
-var words = document.getElementsByClassName("word");
-var wordArray = [];
-var currentWord = 0;
+//slider - LETRAS
 
-words[currentWord].style.opacity = 1;
-for (var i = 0; i < words.length; i++) {
-  splitLetters(words[i]);
-}
+var TxtRotate = function(el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 100) || 4000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
+};
 
-function changeWord() {
-  var cw = wordArray[currentWord];
-  var nw =
-    currentWord == words.length - 1 ? wordArray[0] : wordArray[currentWord + 1];
-  for (var i = 0; i < cw.length; i++) {
-    animateLetterOut(cw, i);
+TxtRotate.prototype.tick = function() {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
+
+  if (this.isDeleting) {
+    this.txt = fullTxt.substring(0, this.txt.length - 1);
+  } else {
+    this.txt = fullTxt.substring(0, this.txt.length + 1);
   }
 
-  for (var i = 0; i < nw.length; i++) {
-    nw[i].className = "letter behind";
-    nw[0].parentElement.style.opacity = 1;
-    animateLetterIn(nw, i);
+  this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+  var that = this;
+  var delta = 150 - Math.random() * 100;
+
+  if (this.isDeleting) { delta /= 2; }
+
+  if (!this.isDeleting && this.txt === fullTxt) {
+    delta = this.period;
+    this.isDeleting = true;
+  } else if (this.isDeleting && this.txt === '') {
+    this.isDeleting = false;
+    this.loopNum++;
+    delta = 1000;
   }
 
-  currentWord = currentWord == wordArray.length - 1 ? 0 : currentWord + 1;
-}
+  setTimeout(function() {
+    that.tick();
+  }, delta);
+};
 
-function animateLetterOut(cw, i) {
-  setTimeout(function () {
-    cw[i].className = "letter out";
-  }, i * 80);
-}
-
-function animateLetterIn(nw, i) {
-  setTimeout(function () {
-    nw[i].className = "letter in";
-  }, 340 + i * 80);
-}
-
-function splitLetters(word) {
-  var content = word.innerHTML;
-  word.innerHTML = "";
-  var letters = [];
-  for (var i = 0; i < content.length; i++) {
-    var letter = document.createElement("span");
-    letter.className = "letter";
-    letter.innerHTML = content.charAt(i);
-    word.appendChild(letter);
-    letters.push(letter);
+window.onload = function() {
+  var elements = document.getElementsByClassName('txt-rotate');
+  for (var i=0; i<elements.length; i++) {
+    var toRotate = elements[i].getAttribute('data-rotate');
+    var period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+      new TxtRotate(elements[i], JSON.parse(toRotate), period);
+    }
   }
-
-  wordArray.push(letters);
-}
-
-changeWord();
-setInterval(changeWord, 4000);
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #F26921 }";
+  document.body.appendChild(css);
+};
 
 //scroll
 function scrollTop() {
